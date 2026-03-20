61aac7a50f47bd734ffbea86a2952f62de4702a9
Thiago Tavares <thiago.tlopess@gmail.com>
2026-03-20 18:25:00 -0300

    att

--- teste_arfima.R
+++ teste_arfima.R
@@ -1,24 +1,34 @@
 
 library(rugarch)
 
-rssit <- rssi_train_list$Tinovi02$rssi
+rssit <- rssi_train_list$Tinovi01$rssi
 
-temp <- rssi_train_list$Tinovi02$airtemp
+temp <- rssi_train_list$Tinovi01$airtemp
 
-hum <- rssi_train_list$Tinovi02$airhum
+hum <- rssi_train_list$Tinovi01$airhum
 
 Xreg <- as.matrix(cbind(temp, hum))
 
 best_fit_arfima <- autoarfima(rssit, ar.max = 2, ma.max = 2, 
                               criterion = "AIC",
                               method = "full", arfima = T,
-                              external.regressors = as.matrix(temp),
+                              external.regressors = Xreg,
                               solver = "solnp", return.all = FALSE)
 
 
 best_fit_arfima
+fdGPH(rssit, bandw.exp = 0.5)
+fdSperio(rssit, bandw.exp = 0.5, beta = 0.9)
+acf(rssit)
+plot.ts(rssit)
+
+tseries::adf.test(rssit)
+tseries::pp.test(rssit)
 
 
+y<-diff(rssit)
+plot.ts(y)
+tseries::adf.test(y)
 # spec <- ugarchspec(
 #   variance.model = list(model = "sGARCH", garchOrder = c(1,1)),
 #   mean.model     = list(
